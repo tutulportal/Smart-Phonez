35bd091f178848b6a0c517d09489af6b23f28fdf
tutulportal <tutulvpn3@gmail.com>
2022-11-25 01:38:36 +0600

    login & register page design ready

---
+++ src/pages/Register/Register.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Register = () => {
+    return (
+        <div className='w-full flex flex-col justify-center items-center py-10'>
+            <div className="max-w-md p-4 rounded-md shadow sm:p-8 dark:bg-gray-900 dark:text-gray-100">
+                <h2 className="mb-3 text-3xl font-semibold text-center">Register</h2>
+                <p className="text-sm text-center dark:text-gray-400">Already have account?
+                    <Link to="/login" rel="noopener noreferrer" className="focus:underline hover:underline">Login here</Link>
+                </p>
+                <form novalidate="" action="" className="space-y-8 ng-untouched ng-pristine ng-valid pt-4">
+                    <div className="space-y-4">
+                        <div className="space-y-2">
+                            <label for="name" className="block text-sm">Full Name</label>
+                            <input type="text" name="name" id="name" placeholder="Jhon Doe" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
+                        </div>
+                        <div className="space-y-2">
+                            <label for="email" className="block text-sm">Email address</label>
+                            <input type="email" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
+                        </div>
+                        <div className="space-y-2">
+                            <div className="flex justify-between">
+                                <label for="password" className="text-sm">Password</label>
+                                <a rel="noopener noreferrer" href="#" className="text-xs hover:underline dark:text-gray-400">Forgot password?</a>
+                            </div>
+                            <input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100 focus:dark:border-violet-400" />
+                        </div>
+                    </div>
+                    <button type="button" className="w-full px-8 py-3 font-semibold rounded-md dark:bg-violet-400 dark:text-gray-900">Sign in</button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
--- src/routes/routes/router.js
+++ src/routes/routes/router.js
@@ -1,7 +1,9 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Main from '../../layouts/Main'
 import Home from '../../pages/Home/Home/Home';
+import Login from '../../pages/Login/Login';
 import NotFound from '../../pages/NotFound/NotFound';
+import Register from '../../pages/Register/Register';
 
 const router = createBrowserRouter([
     {
@@ -11,6 +13,14 @@ const router = createBrowserRouter([
             {
                 path: '/',
                 element: <Home/>
+            },
+            {
+                path: '/login',
+                element: <Login/>
+            },
+            {
+                path: '/register',
+                element: <Register/>
             }
         ]
     },
